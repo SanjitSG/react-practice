e2410aa0bd4c79fe0dff67fb25a64d85b84fa8c6
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-03-22 16:01:31 +0530

    practice-01

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("h1", { id: "parent" }, [
+  React.createElement("h1", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "im heading"),
+    React.createElement("h1", { id: "heading" }, "im heading2"),
+  ]),
+  React.createElement("h1", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "im heading"),
+    React.createElement("h1", { id: "heading" }, "im heading2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);

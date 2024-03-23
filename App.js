c8cfa8ea8cc6788504b6a0b30b2149f7f9682c57
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-03-23 22:23:43 +0530

    parcel

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "parent" }, [
   React.createElement("h1", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "im heading"),
+    React.createElement("h1", { id: "heading" }, "im heading 🚀"),
     React.createElement("h1", { id: "heading" }, "im heading2"),
   ]),
   React.createElement("h1", { id: "child2" }, [
